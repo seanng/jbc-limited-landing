6bd4ab3e077b1cf722d021d6e5e6c50b7a68bb30
seanng <shonum@gmail.com>
2020-05-15 17:26:24 +0800

    update site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'JBC Limited Official Website',
+    description: 'Your Partner in Global Sourcing Solutions',
   },
   plugins: [
     {
