7c6d7e53c328274c47e6152d5a77b9a71c63af45
seanng <shonum@gmail.com>
2020-07-23 11:00:48 +0800

    update og image to jpg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -39,7 +39,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/logo.svg`}
+          content={`${withPrefix('/')}img/logo.jpg`}
         />
       </Helmet>
       <Navbar />
